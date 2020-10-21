3ad62e4b7de5d20e52112b493d5d2ba1189a95c2
bawdeveloppement <baw.developpement@gmail.com>
2020-10-21 17:24:24 +0200

    Add signup component

--- src/components/signup/signup.component.jsx
+++ src/components/signup/signup.component.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import FormInput from '../forms/input/FormInput.component';
+import FormButton from '../forms/button/FormButton.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import './signup.styles.scss';
 
 const SignUp = () => {
-    return <div>SignUp</div>
+    const [displayName, setDisplayName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        if (password !== confirmPassword) {
+            alert('passwords don\'t match');
+            return;
+        }
+
+        try {
+            const { user} = await auth.createUserWithEmailAndPassword(email, password);
+            
+            createUserProfileDocument(user, { displayName });
+
+            setConfirmPassword('');
+            setDisplayName('');
+            setEmail('');
+            setPassword('');
+        } catch(error) {
+            console.error(error);
+        }
+    };
+
+    return <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="signup-form" onSubmit={(e) => handleSubmit(e)}>
+            <FormInput 
+                name="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label="Email"
+                required
+            />
+            <FormInput
+                type="text"
+                name="displayName"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+                label="Display Name"
+                required
+            />
+            <FormInput
+                name="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                label="Password"
+                required
+            />
+            <FormInput
+                name="confirmPassword"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                label="Confirm Password"
+                required
+            />
+            <FormButton>SIGN UP</FormButton>
+        </form>
+    </div>
 }
 
 export default SignUp
