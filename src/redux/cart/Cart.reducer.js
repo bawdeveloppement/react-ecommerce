e1e571be88fcf952db9a26e6abd069f63a2eac3e
bawdeveloppement <baw.developpement@gmail.com>
2020-10-23 13:14:04 +0200

    Cart Redux Store

---
+++ src/redux/cart/Cart.reducer.js
@@ -0,0 +1,25 @@
+import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './Cart.constants'
+
+const INITIAL_STATE = {
+    hidden : false,
+    items: []
+};
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            }
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: [...state.items, action.payload]
+            }
+        default:
+            return state;
+    }
+}
+
+export default cartReducer;
