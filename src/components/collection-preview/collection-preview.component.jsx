1a3544cababb1f93acbe173c39ebc5b9574d3e64
bawdeveloppement <baw.developpement@gmail.com>
2020-10-23 13:18:10 +0200

    use preview layout for both preview somes items & the entire collection if is the correspond route

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
+import Link from '../linknav/linknav.component'
+import { useLocation } from 'react-router';
 
-const PreviewCollection = ({ title, items, restrict }) => (
+const PreviewCollection = ({ title, items, restrict, routeName }) => {
+    const { pathname } = useLocation();
+    const isActive = pathname.includes(routeName);
+    return (
     <div className="collection-preview">
-        <h1 className="title">{title}</h1>
+        <h1 className={`title ${isActive ? 'active' : ''}`}>{ !isActive ? <Link to={routeName} className="title-link">{title}</Link> : title }</h1>
         <div className="preview">
             {items
-            .filter((item, idx) => restrict && typeof(restrict) == "number" ? idx < 4 : idx)
-            .map(({ id, ...otherItemProps}) => <CollectionItem key={id} {...otherItemProps} /> )}
+            .filter((item, idx) => restrict && typeof(restrict) == "number" && restrict !== 0 ? idx < 4 : idx)
+            .map(item => <CollectionItem key={item.id} item={item} /> )}
         </div>
     </div>
-);
+    )
+};
 
 export default PreviewCollection;
