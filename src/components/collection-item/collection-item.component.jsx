c35eb12e22c9a7aa70c957953ca15007bd924f6c
bawdeveloppement <baw.developpement@gmail.com>
2020-10-24 14:10:11 +0200

    Add currency token

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -18,7 +18,7 @@ const CollectionItem = ({item}) => {
         />
         <div className="collection-footer">
             <span className="name">{name}</span>
-            <span className="price">{price}</span>
+            <span className="price">{price} $</span>
         </div>
         <FormButton isInverted onClick={() => dispatch(addItemToCart(item))}>ADD TO CART</FormButton>
     </div>
