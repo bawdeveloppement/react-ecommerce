7d0689bd91a5305aa0fc0649559d1929144c233a
bawdeveloppement <baw.developpement@gmail.com>
2020-10-24 14:09:28 +0200

    Change itemsCount to count all item quantity with reduce method

--- src/components/cart/icon/CartIcon.component.jsx
+++ src/components/cart/icon/CartIcon.component.jsx
@@ -8,13 +8,18 @@ import './CartIcon.styles.scss';
 
 export default function CartIcon () {
     const dispatch = useDispatch();
-    const { items } = useSelector(state => state.cart);
+    // Reduce the items in cart to get the final count of items based on the quantity of each item
+    const itemsCount = useSelector(
+        ({ cart }) => cart.items.reduce(
+            (acculatedQuantity, cartItem) => acculatedQuantity + cartItem.quantity, 0    
+    ));
+
     function toggleCart () {
         dispatch(toggleCartHidden())
     }
 
     return <div className="cart-icon" onClick={toggleCart}>
         <ShopppingIcon />
-        <div className="item-count">{items.length}</div>
+        <div className="item-count">{itemsCount}</div>
     </div>
 }
