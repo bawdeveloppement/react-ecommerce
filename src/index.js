d7242113aecbf694368405980f90eb8fc0f17c56
bawdeveloppement <baw.developpement@gmail.com>
2020-10-24 14:08:30 +0200

    Add mixins & breakpoints var for handling responsive

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+//#region Base dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+//#endregion
 //#region Redux
 import { Provider as ReduxProvider } from 'react-redux'
 import RootStore from './redux/Root.store';
+//#endregion
+//#region Components
+import App from './App';
+//#endregion
+//#region Styles & StylesRules
+import './index.css';
+import './assets/scss/_mixins.scss'
+import './assets/scss/_breakpoints.scss'
 //#endregion
 
 ReactDOM.render(
